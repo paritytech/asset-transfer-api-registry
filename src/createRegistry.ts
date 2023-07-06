603ddbecd66c38618cad4e4a901f12ee8980a16e
Cameron Marshall <cameron.marshall12@gmail.com>
2023-07-06 13:49:49 -0400

    adds system chain foreign assets info to registry (#10)
    * add call to get foreignAssetsInfo for asset hub system chains
    
    * remove newline

--- src/createRegistry.ts
+++ src/createRegistry.ts
@@ -63,6 +63,18 @@ interface AssetsInfo {
 	[key: string]: string;
 }
 
+type ForeignAssetStorageKeyData = [
+	{ parents: number, interior: { X2: [{ Parachain: string | undefined }, { GeneralIndex: string }] } }
+];
+type ForeignAssetMetadata =  { deposit: string, name: string, symbol: string, decimals: string, isFrozen: boolean };
+
+interface ForeignAssetsInfo {
+	[key: string]: {
+		symbol: string,
+		multiLocation: string
+	}
+}
+
 /**
  * Fetch chain token and spec info.
  *
@@ -105,9 +117,11 @@ const fetchChainInfo = async (
 	const specNameStr = specName.toString();
 
 	let assetsInfo: AssetsInfo = {};
+	let foreignAssetsInfo: ForeignAssetsInfo = {};
 
-	if (specNameStr === 'statemine' || specNameStr === 'statemint') {
+	if (specNameStr === 'westmint' || specNameStr === 'statemine' || specNameStr === 'statemint') {
 		assetsInfo = await fetchSystemParachainAssetInfo(api);
+		foreignAssetsInfo = await fetchSystemParachainForeignAssetInfo(api);
 	}
 
 	await api.disconnect();
@@ -115,6 +129,7 @@ const fetchChainInfo = async (
 	return {
 		tokens,
 		assetsInfo,
+		foreignAssetsInfo,
 		specName: specNameStr,
 		assetsPalletInstance: assetsPallet ? assetsPallet.index.toString() : null,
 	};
@@ -161,7 +176,6 @@ const createChainRegistryFromRelay = async (
 	registry: TokenRegistry
 ): Promise<void> => {
 	const res = await fetchChainInfo(endpoint, true);
-	console.log('Res result: ', res);
 	if (res !== null) {
 		registry[chainName]['0'] = res;
 	}
@@ -222,6 +236,38 @@ const fetchSystemParachainAssetInfo = async (
 	}
 
 	return assetsInfo;
+};
+
+const fetchSystemParachainForeignAssetInfo = async (
+	api: ApiPromise
+): Promise<ForeignAssetsInfo> => {
+	const foreignAssetsInfo: ForeignAssetsInfo = {};
+
+	if (api.query.foreignAssets !== undefined) {
+		for (const [assetStorageKeyData] of await api.query.foreignAssets.asset.entries()) {
+			const assetData = assetStorageKeyData.toHuman();
+
+			if (assetData) {
+				const foreignAssetData = assetData as ForeignAssetStorageKeyData;
+				const id = parseInt(foreignAssetData[0].interior.X2[1].GeneralIndex);
+				const assetMetadata = (await api.query.foreignAssets.metadata(id)).toHuman();
+
+				if (assetMetadata) {
+					const metadata = assetMetadata as ForeignAssetMetadata;
+					const assetSymbol = metadata.symbol;
+	
+					if (assetSymbol != undefined) {
+						foreignAssetsInfo[id] = {
+							symbol: assetSymbol,
+							multiLocation: assetData as string
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return foreignAssetsInfo;
 };
 
 main()
