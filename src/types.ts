34b290f961c434190fb66ff7e59ef184857bc909
Tarik Gul <47201679+TarikGul@users.noreply.github.com>
2023-09-14 13:18:36 -0700

    fix!: remove `xcAssets` key and replace the data to fit in each parachains key under their relay chain (#57)

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,21 @@
 // Copyright 2023 Parity Technologies (UK) Ltd.
 
+import type { AnyJson } from '@polkadot/types/types';
+
 export type TokenRegistry = {
-	polkadot: {};
-	kusama: {};
-	westend: {};
+	polkadot: ChainInfo;
+	kusama: ChainInfo;
+	westend: ChainInfo;
+};
+
+export type ChainInfo = {
+	[key: string]: {
+		assetsInfo: AssetsInfo;
+		specName: string;
+		foreignAssetsInfo: ForeignAssetsInfo;
+		poolPairsInfo: PoolPairsInfo;
+		xcAssetsData?: SanitizedXcAssetsData[];
+	};
 };
 
 export type PoolPairsInfo = {
@@ -42,3 +54,43 @@ export interface PoolInfo {
 export interface ParaIds {
 	[key: string]: number[];
 }
+
+export type XcAssets = {
+	polkadot: XcAssetsInfo[];
+	kusama: XcAssetsInfo[];
+};
+
+export type XcAssetsInfo = {
+	relayChain: string;
+	paraID: number;
+	id: string;
+	xcAssetCnt: string;
+	data: XcAssetsData[];
+};
+
+export type XcAssetsData = {
+	paraID: number;
+	relayChain: string;
+	nativeChainId: string;
+	symbol: string;
+	decimals: number;
+	interiorType: string;
+	xcmV1Standardized: XcAssetXcmStandardized[];
+	xcmV1MultiLocationByte: boolean;
+	xcmV1MultiLocation: AnyJson;
+	asset: { ForeignAsset: string } | string;
+	source: string[];
+};
+
+export type SanitizedXcAssetsData = {
+	paraID: number;
+	nativeChainId: string;
+	symbol: string;
+	decimals: number;
+	xcmV1MultiLocation: string;
+	asset: { ForeignAsset: string } | string;
+};
+
+export type XcAssetXcmStandardized = {
+	[x: string]: string | number;
+};
