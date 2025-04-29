f508a348c022d93e3cd84a58e4842b0c27778a69
Dominique <dominique@imodworks.io>
2025-04-29 08:52:47 +0200

    feat: add xcAssetsData property with USDC-USDT (#180)

--- src/fetchChainInfo.ts
+++ src/fetchChainInfo.ts
@@ -1,7 +1,9 @@
 // Copyright 2024 Parity Technologies (UK) Ltd.
 
+import { ApiPromise } from '@polkadot/api';
 import { EndpointOption } from '@polkadot/apps-config/endpoints/types.js';
 
+import FinalRegistry from '../docs/registry.json' assert { type: 'json' };
 import { ASSET_HUB_SPEC_NAMES } from './consts.js';
 import { fetchSystemParachainAssetConversionPoolInfo } from './fetchSystemParachainAssetConversionPoolInfo.js';
 import { fetchSystemParachainAssetInfo } from './fetchSystemParachainAssetInfo.js';
@@ -12,6 +14,7 @@ import type {
 	ChainInfoKeys,
 	ForeignAssetsInfo,
 	PoolPairsInfo,
+	SanitizedXcAssetsData,
 } from './types.js';
 import { logWithDate } from './util.js';
 
@@ -58,6 +61,8 @@ export const fetchChainInfo = async (
 			poolPairsInfo = await fetchSystemParachainAssetConversionPoolInfo(api);
 		}
 
+		const xcAssets = await updateChainInfoWithUsdtAsset(api, paraId);
+
 		await api.disconnect();
 
 		return [
@@ -67,10 +72,72 @@ export const fetchChainInfo = async (
 				foreignAssetsInfo,
 				poolPairsInfo,
 				specName: specNameStr,
+				...(xcAssets && xcAssets.length != 0 ? { xcAssetsData: xcAssets } : {}),
 			},
 			endpointOpts.paraId,
 		];
 	} else {
 		return null;
 	}
+};
+
+const updateChainInfoWithUsdtAsset = async (
+	api: ApiPromise,
+	paraId: number,
+): Promise<SanitizedXcAssetsData[] | undefined> => {
+	if (!api?.call?.xcmPaymentApi) return;
+
+	const chainInfoRegistry = FinalRegistry.polkadot[paraId] as ChainInfoKeys;
+
+	// Check if USDT or USDC are already in xcAssetsData
+	if (
+		chainInfoRegistry?.xcAssetsData?.some((item) =>
+			/usdt|usdc/i.test(item.symbol),
+		)
+	) {
+		return;
+	}
+
+	// USDT Location
+	const usdtXcmPath =
+		'{"v3":{"concrete":{"parents":1,"interior":{"x3":[{"parachain":1000},{"palletInstance":50},{"generalIndex":1984}]}}}}';
+	// USDC Location
+	const usdcXcmPath =
+		'{"v3":{"concrete":{"parents":1,"interior":{"x3":[{"parachain":1000},{"palletInstance":50},{"generalIndex":1337}]}}}}';
+
+	const xcAssets: SanitizedXcAssetsData[] = [];
+	// Check acceptable payment assets
+	try {
+		const paymentAssetsV3 =
+			await api.call.xcmPaymentApi.queryAcceptablePaymentAssets(3);
+		const paymentAssetsJson = JSON.parse(JSON.stringify(paymentAssetsV3)) as {
+			ok: string[];
+		};
+		const assetDict: Record<string, string> = {
+			[usdtXcmPath]: 'USDT',
+			[usdcXcmPath]: 'USDC',
+		};
+		// Check if USDT or USDC are one of the acceptable payment assets
+		for (const item of paymentAssetsJson.ok) {
+			if (
+				JSON.stringify(item) == usdtXcmPath ||
+				JSON.stringify(item) == usdcXcmPath
+			) {
+				xcAssets.push({
+					paraID: paraId,
+					nativeChainID: 'asset-hub-polkadot',
+					symbol: assetDict[JSON.stringify(item)],
+					decimals: 6,
+					xcmV1MultiLocation: usdcXcmPath,
+					assetHubReserveLocation: '{"parents":"0","interior":{"Here":""}}',
+				} as SanitizedXcAssetsData);
+			}
+		}
+	} catch (error) {
+		console.error(
+			`Error fetching XCM payment assets for paraId ${paraId}:`,
+			error,
+		);
+	}
+	return xcAssets;
 };
