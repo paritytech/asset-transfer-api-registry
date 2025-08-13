28d7f4ac7e947e14c10080eed5fb4f056e8c0a42
Andrew Ahlers <andrew.ahlers@parity.io>
2025-08-13 13:33:28 +0200

    fix: Exit with exit code 1 when there is an error.
    The scheduled job has been repeatedly failing in silence due to the
    removal of https://github.com/colorfulnotion/xcm-global-registry and https://cdn.jsdelivr.net/gh/colorfulnotion/xcm-global-registry/metadata/xcmgar.json
    but this was going uncaught.

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,13 @@ import { PROD_REGISTRY_FILE_PATH } from './consts.js';
 import { main } from './createRegistry.js';
 import type { TokenRegistry } from './types.js';
 
-main(PROD_REGISTRY_FILE_PATH, REGISTRY as unknown as TokenRegistry)
-	.catch((err) => console.error(err))
-	.finally(() => process.exit());
+void (async () => {
+	try {
+		await main(PROD_REGISTRY_FILE_PATH, REGISTRY as unknown as TokenRegistry);
+	} catch (err) {
+		console.error(err);
+		process.exit(1);
+	} finally {
+		process.exit();
+	}
+})();
